93a5bb9664a0c2df5f6a8545f4ff7332d6d1a2ae
Maimoon Hossain <ashikasitosh740@gmail.com>
2024-02-25 21:10:58 +0600

    store and reducers updated

--- react-redux-bank/src/store.js
+++ react-redux-bank/src/store.js
@@ -1,65 +1,6 @@
 import { combineReducers, createStore } from 'redux';
-
-const initialStateAccount = {
-  balance: 0,
-  loan: 0,
-  loanPurpose: '',
-};
-
-const initialStateCustomer = {
-  fullName: '',
-  nationalID: '',
-  createdAt: '',
-};
-
-function accountReducer(state = initialStateAccount, action) {
-  switch (action.type) {
-    case 'account/deposit':
-      return {
-        ...state,
-        balance: state.balance + action.payload,
-      };
-    case 'account/withdraw':
-      return {
-        ...state,
-        balance: state.balance - action.payload,
-      };
-    case 'account/requestLoan':
-      if (state.loan > 0) return state;
-      return {
-        ...state,
-        loan: action.payload.amount,
-        loanPurpose: action.payload.purpose,
-        balance: state.balance + action.payload.amount,
-      };
-    case 'account/payLoan':
-      return {
-        ...state,
-        balance: state.balance - state.loan,
-        loanPurpose: '',
-        loan: 0,
-      };
-    default:
-      return state;
-  }
-}
-
-function customerReducer(state = initialStateCustomer, action) {
-  switch (action.type) {
-    case 'customer/createCustomer':
-      return {
-        ...state,
-        fullName: action.payload.fullName,
-        nationalID: action.payload.nationalID,
-        createdAt: action.payload.createdAt,
-      };
-    case 'customer/updateName':
-      return { ...state, fullName: action.payload };
-
-    default:
-      return state;
-  }
-}
+import accountReducer from './features/accounts/accountSlice';
+import customerReducer from './features/customers/customerSlice';
 
 const rootReducer = combineReducers({
   account: accountReducer,
@@ -67,6 +8,8 @@ const rootReducer = combineReducers({
 });
 
 const store = createStore(rootReducer);
+
+export default store;
 
 // store.dispatch({ type: 'account/deposit', payload: 500 });
 // store.dispatch({ type: 'account/withdraw', payload: 100 });
@@ -84,49 +27,19 @@ const store = createStore(rootReducer);
 
 // action creators
 
-function deposit(amount) {
-  return { type: 'account/deposit', payload: amount };
-}
+// store.dispatch(deposit(500));
+// console.log(store.getState());
 
-function withdraw(amount) {
-  return { type: 'account/withdraw', payload: amount };
-}
+// store.dispatch(withdraw(100));
+// console.log(store.getState());
 
-function requestLoan(amount, purpose) {
-  return { type: 'account/requestLoan', payload: { amount, purpose } };
-}
+// store.dispatch(requestLoan(1000, 'Buy a car'));
+// console.log(store.getState());
 
-function payLoan() {
-  return { type: 'account/payLoan' };
-}
-
-store.dispatch(deposit(500));
-console.log(store.getState());
-
-store.dispatch(withdraw(100));
-console.log(store.getState());
-
-store.dispatch(requestLoan(1000, 'Buy a car'));
-console.log(store.getState());
-
-store.dispatch(payLoan());
-console.log(store.getState());
+// store.dispatch(payLoan());
+// console.log(store.getState());
 // This is a simple example of a Redux store. It has a reducer that manages the state of the store, and it has action creators that dispatch actions to the store.
 
-function createCustomer(fullName, nationalID) {
-  return {
-    type: 'customer/createCustomer',
-    payload: { fullName, nationalID, createdAt: new Date().toISOString() },
-  };
-}
-
-function updateCustomer(fullName) {
-  return {
-    type: 'customer/updateCustomer',
-    payload: fullName,
-  };
-}
-
-store.dispatch(deposit(500));
-store.dispatch(createCustomer('John Doe', '123456789'));
-console.log(store.getState());
+// store.dispatch(deposit(500));
+// store.dispatch(createCustomer('John Doe', '123456789'));
+// console.log(store.getState());
